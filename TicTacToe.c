54bc7eb6a77b775fffa7fcd06094a9713f4da434
prithzzz <prithikasoundararajan@gmail.com>
2025-04-20 12:21:34 +0530

    Completed

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <time.h>
 
 int board[3][3]; 
@@ -18,28 +18,42 @@ void printWinner(char winner);
 int main()
 {
     char winner = ' ';
-
-    resetBoard();
-
-    while(winner == ' ' && checkEmptySpaces() != 0)
+    int response;
+    do
     {
+        winner = ' ';
+        response = ' ';
+        resetBoard();
+        printf("\nWelcome to Tic Tac Toe!\n");
+        printf("You are 'X' and the computer is 'O'\n");
+
+        while(winner == ' ' && checkEmptySpaces() != 0)
+        {
+            printBoard();
+            playerMove();
+            winner = checkWinner();
+            if(winner != ' ' || checkEmptySpaces() == 0)
+            {
+                break;
+            }
+            computerMove();
+            winner = checkWinner();
+            if(winner != ' ' || checkEmptySpaces() == 0)
+            {
+                break;
+            }
+        }
         printBoard();
-        playerMove();
-        winner = checkWinner();
-        if(winner != ' ' || checkEmptySpaces() == 0)
-        {
-            break;
-        }
-        computerMove();
-        winner = checkWinner();
-        if(winner != ' ' || checkEmptySpaces() == 0)
-        {
-            break;
-        }
-    }
-    
-    printBoard();
-    printWinner(winner);
+        printWinner(winner);
+
+        printf("Do you want to play again?(Y/N):");
+        scanf(" %c",&response);
+        response = toupper(response);
+        
+    } while(response == 'Y');
+
+    printf("Thanks for playing!\n");
+
     return 0;
 }
 
@@ -94,7 +108,7 @@ void playerMove()
 
         if(board[x][y] != ' ')
         {
-            printf("Invalid move!");
+            printf("Invalid move!\n");
         }
         else
         {
@@ -106,7 +120,14 @@ void playerMove()
 
 void computerMove()
 {
-
+    srand(time(0));
+    int x,y;
+    while(board[x][y] != ' ')
+    {
+        x = rand() % 3;
+        y = rand() % 3;
+    }
+    board[x][y] = Computer;
 }
 
 char checkWinner()
@@ -114,12 +135,35 @@ char checkWinner()
     //checking rows
     for(int i=0; i<3; i++)
     {
-        if(board[i][0] == board[i][1] || board[i][0] == board[i][1])
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2])
         {
-            //before continuing, connect with github!!
+            return board[i][0];
         }
-
     }
+    //checking columns
+    for(int i=0; i<3; i++)
+    {
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i])
+        {
+            return board[0][i];
+        }
+    }
+    //checking diagnols
+    for(int i=0; i<3; i++)
+    {
+        if(board[0][0] == board[1][1] && board[0][0] == board[2][2])
+        {
+            return board[0][0];
+        }
+    }
+    for(int i=0; i<3; i++)
+    {
+        if(board[0][2] == board[1][1] && board[0][2] == board[2][0])
+        {
+            return board[0][2];
+        }
+    }
+    return ' ';
 }
 
 void printWinner(char winner)
